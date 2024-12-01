189f3b1fbc0b90f3a769ca9921d5d5bf2004d187
patientx <patient.x@gmail.com>
2024-12-01 21:21:22 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ set "startTime=%time: =0%"
 
 cls
 echo -------------------------------------------------------------
-Echo ******************* F5-TTS-ONNX with GUI INSTALL *******************
+Echo *************** F5-TTS-ONNX with GUI INSTALL ****************
 echo -------------------------------------------------------------
 echo.
 echo  ::  %time:~0,8%  ::  - Setting up the virtual enviroment
@@ -26,8 +26,6 @@ echo  ::  %time:~0,8%  ::  Beginning installation ...
 echo.
 echo  ::  %time:~0,8%  ::  - Installing required packages
 pip install -r requirements.txt --quiet
-echo  ::  %time:~0,8%  ::  - Downloading default ONNX models of F5-TTS
-pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu118 --quiet
 echo. 
 echo  ::  %time:~0,8%  ::  - Downloading vocab file for F5-TTS
 curl -s -L https://huggingface.co/SWivid/F5-TTS/resolve/main/F5TTS_Base/vocab.txt > models\vocab.txt
@@ -36,11 +34,10 @@ set "endTime=%time: =0%"
 set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
 set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
 set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
-echo ..................................................... 
+echo ............................................................... 
 echo *** Installation is completed in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1% . 
-echo *** You can use "comfyui.bat" to start the app later. 
-echo ..................................................... 
+echo *** Please download models from this url and extract them into models\onnx folder. 
+echo *** https://drive.google.com/file/d/1_QNdX-6l8iwDF9c6HxOxW5bpCB6HJGnR/view?usp=sharing
+echo ...............................................................
 echo.
-echo *** Starting the Comfyui-ZLUDA for the first time, please be patient...
-echo.
-.\zluda\zluda.exe -- python main.py --auto-launch --use-quad-cross-attention
+pause
